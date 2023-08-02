ef11711888b40e89e3b8d1d69dd9eb948987e5b1
P DHARANI KUMAR <dkumar293095@gmail.com>
2023-08-02 09:29:08 +0530

    admin ui

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,9 +5,13 @@ import "react-toastify/dist/ReactToastify.css";
 
 function Main() {
   return (
-    <div className="Main">
+    <div className="Main" class="footer-container">
       <UserManagementInterface />
       <ToastContainer autoClose={1000} />
+      {/* Footer */}
+      <p className="footer">
+        Greekstart Forntend Challenge:- AdminUI &#174; P DHARANI KUMAR | 2023
+      </p>
     </div>
   );
 }
